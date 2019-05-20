989c7d1d036e429d32da1838f943b2b5e488ae25
Alec Leamas <leamas.alec@gmail.com>
2019-05-20 10:00:39 +0200

    Bundle msvc/mingw API libs (#36).
    Old code refererred to libraries outside the git tree, a
    plain bug. Bundle the build dependencies.

--- ci/docker-build-flatpak.sh
+++ ci/docker-build-flatpak.sh
@@ -11,8 +11,8 @@ cd /opencpn-ci
 su -c "dnf install -y sudo cmake gcc-c++ flatpak-builder flatpak make tar"
 flatpak remote-add --user --if-not-exists flathub \
     https://flathub.org/repo/flathub.flatpakrepo
- flatpak install --user  -y \
-        https://leamas.fedorapeople.org/opencpn/opencpn.flatpakref
+flatpak install --user  -y \
+        http://opencpn.duckdns.org/opencpn/opencpn.flatpakref
 flatpak install --user -y  flathub org.freedesktop.Sdk//18.08 
 rm -rf build && mkdir build && cd build
 cmake -DOCPN_FLATPAK=ON ..
