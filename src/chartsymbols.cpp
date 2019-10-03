ed7e9589fe1dd86c22e976344fdef421a7aeb93f
dsr <bdbcat@yahoo.com>
2019-10-02 22:54:18 -0400

    Correct Raster symbol texture parameter setup.

--- src/chartsymbols.cpp
+++ src/chartsymbols.cpp
@@ -951,19 +951,15 @@ int OE_ChartSymbols::LoadRasterFileForColorTable( int tableNo, bool flush )
             }
 
             glBindTexture(g_oe_texture_rectangle_format, oe_rasterSymbolsTexture);
-            glTexParameteri(g_oe_texture_rectangle_format, GL_TEXTURE_BASE_LEVEL, 0);
-            glTexParameteri(g_oe_texture_rectangle_format, GL_TEXTURE_MAX_LEVEL, 0);
-             glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
-             glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
-
-            /* unfortunately this texture looks terrible with compression */
-            glTexImage2D(g_oe_texture_rectangle_format, 0, GL_RGBA, w, h,
+            GLuint format = GL_RGBA;
+            glTexImage2D(g_oe_texture_rectangle_format, 0, format, w, h,
                          0, GL_RGBA, GL_UNSIGNED_BYTE, e);
 
-//              glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
-//              glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
-//              glTexParameteri( g_texture_rectangle_format, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-//              glTexParameteri( g_texture_rectangle_format, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+            glTexParameteri(g_oe_texture_rectangle_format, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameteri(g_oe_texture_rectangle_format, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+            
+            glTexParameteri(g_oe_texture_rectangle_format, GL_TEXTURE_MAG_FILTER,  GL_NEAREST );   // No mipmapping
+            glTexParameteri(g_oe_texture_rectangle_format, GL_TEXTURE_MIN_FILTER,  GL_NEAREST );
 
             oe_rasterSymbolsTextureSize = wxSize(w, h);
 
