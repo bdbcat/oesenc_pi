4507ca2727f519e96c2aca3343376c152e8cd567
Alec Leamas <leamas.alec@nowhere.net>
2020-03-04 20:56:32 +0100

    fix install paths, part 3.

--- ci/circleci-build-macos.sh
+++ ci/circleci-build-macos.sh
@@ -31,10 +31,9 @@ echo 'export PATH="/usr/local/opt/gettext/bin:$PATH"' >> ~/.bash_profile
 rm -rf build && mkdir build && cd build
 CI_BUILD=ON
 cmake -DOCPN_CI_BUILD=$CI_BUILD \
-  -DOCPN_USE_LIBCPP=ON \
   -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wx312_opencpn50_macos109/bin/wx-config \
   -DwxWidgets_CONFIG_OPTIONS="--prefix=/tmp/wx312_opencpn50_macos109" \
-  -DCMAKE_INSTALL_PREFIX=/tmp/opencpn -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
+  -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
   ..
 make -sj2
 make package
