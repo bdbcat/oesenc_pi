c683116a90f941305b0a0eca6d17d9839775d755
dsr <bdbcat@yahoo.com>
2020-03-13 20:25:35 -0400

    Trusty build #7

--- ci/circleci-build-trusty.sh
+++ ci/circleci-build-trusty.sh
@@ -10,15 +10,14 @@ sudo apt-get install devscripts equivs
 mkdir  build
 cd build
 mk-build-deps ../ci/control
-#sudo apt install  ./*all.deb
 sudo dpkg -i ./*all.deb   || :
 sudo apt-get --allow-unauthenticated install -f
 
-#if [ -n "$BUILD_GTK3" ]; then
-#    sudo update-alternatives --set wx-config \
-#        /usr/lib/*-linux-*/wx/config/gtk3-unicode-3.0
-#fi
+if [ -n "$BUILD_GTK3" ]; then
+    sudo update-alternatives --set wx-config \
+        /usr/lib/*-linux-*/wx/config/gtk3-unicode-3.0
+fi
 
-#cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
-#make -sj2
-#make package
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+make -sj2
+make package
