e07b696fdf6ac5fd2d8d2c9faf186d773a75a73f
Alec Leamas <leamas.alec@nowhere.net>
2020-03-22 11:59:49 +0100

    ci: Restore name of tarball.

--- ci/circleci-upload.sh
+++ ci/circleci-upload.sh
@@ -41,7 +41,6 @@ tag=$(git tag --contains HEAD)
 xml=$(ls $HOME/project/build/*.xml)
 tarball=$(ls $HOME/project/build/*.tar.gz)
 tarball_basename=${tarball##*/}
-tarball_name=${tarball_basename%.tar.gz}
 
 # extract the project name for a filename.  e.g. oernc-pi... sets PROJECT to  "oernc"
 PROJECT=${tarball_basename%%_pi*}
@@ -49,6 +48,7 @@ PROJECT=${tarball_basename%%_pi*}
 source $HOME/project/build/pkg_version.sh
 test -n "$tag" && VERSION="$tag" || VERSION="${VERSION}.${commit}"
 test -n "$tag" && REPO="$STABLE_REPO" || REPO="$UNSTABLE_REPO"
+tarball_name=${PROJECT}-${PKG_TARGET}-${PKG_TARGET_VERSION}-tarball
 
 sudo sed -i -e "s|@pkg_repo@|$REPO|" $xml
 sudo sed -i -e "s|@name@|$tarball_name|" $xml
