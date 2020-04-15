35a0e20d4613679004c43a83768a5984c28ead65
dsr <bdbcat@yahoo.com>
2020-04-14 22:17:03 -0400

    Improve reliability of merged tarball creation. #14

--- ci/travis-build-raspbian-armhf.sh
+++ ci/travis-build-raspbian-armhf.sh
@@ -139,7 +139,6 @@ cat ~/$xml
 #sudo tar -rf $tarball_tar metadata.xml
 #sudo gzip $tarball_tar
 
-cd build
 sudo tar xf $tarball
 tar_dir=${tarball%%.tar.gz}
 ls -la
@@ -147,7 +146,6 @@ ls -la $tar_dir
 sudo cp $xml $tar_dir/metadata.xml
 tar_dir_here=${tar_dir##*/}
 sudo tar czf $tarball $tar_dir_here
-cd ..
 
 cloudsmith push raw --republish --no-wait-for-sync \
     --name ${PROJECT}-${PKG_TARGET}-${PKG_TARGET_VERSION}-metadata \
