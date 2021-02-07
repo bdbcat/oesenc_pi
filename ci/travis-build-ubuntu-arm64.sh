c6f8f5ff08a660f299938cd8d50e07f5109f761e
Alec Leamas <leamas.alec@nowhere.nt>
2021-02-07 10:21:23 +0100

    ci: Simplify  and cleanup docker handling.

--- ci/travis-build-ubuntu-arm64.sh
+++ ci/travis-build-ubuntu-arm64.sh
@@ -6,31 +6,9 @@ set -xe
 
 sudo apt-get -qq update
 
-# Start up the docker container
+# Create build script
 #
-DOCKER_SOCK="unix:///var/run/docker.sock"
-echo "DOCKER_OPTS=\"-H tcp://127.0.0.1:2375 -H $DOCKER_SOCK -s devicemapper\"" \
-    | sudo tee /etc/default/docker > /dev/null
-sudo systemctl restart docker.service
-sleep 5;
-
-#docker run --rm --privileged multiarch/qemu-user-static:register --reset
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-
-docker run --privileged -d -ti -e "container=docker" \
-        -v "$TRAVIS_BUILD_DIR:/ci-source:rw" \
-        -e "CLOUDSMITH_STABLE_REPO=$CLOUDSMITH_STABLE_REPO" \
-        -e "CLOUDSMITH_BETA_REPO=$CLOUDSMITH_BETA_REPO" \
-        -e "CLOUDSMITH_UNSTABLE_REPO=$CLOUDSMITH_UNSTABLE_REPO" \
-        -e "CIRCLE_BUILD_NUM=$CIRCLE_BUILD_NUM" \
-        -e "TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER" \
-    $DOCKER_IMAGE /bin/bash
-DOCKER_CONTAINER_ID=$(sudo docker ps | awk '/ubuntu/ {print $1}')
-
-
-# Run the build
-#
-cat >build.sh << "EOF"
+cat >$TRAVIS_BUILD_DIR/build.sh << "EOF"
 apt -qq update
 apt -y -q install git cmake build-essential \
    cmake gettext wx-common libwxgtk3.0-dev \
@@ -49,11 +27,19 @@ cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make tarball
 EOF
 
-docker exec -ti $DOCKER_CONTAINER_ID \
-    /bin/bash -xec "bash -xe /ci-source/build.sh"
-docker stop $DOCKER_CONTAINER_ID
-docker rm -v $DOCKER_CONTAINER_ID
-rm -f build.sh
+
+# Run script in docker
+#
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker run --privileged -d -ti -e "container=docker" \
+        -v "$TRAVIS_BUILD_DIR:/ci-source:rw" \
+        -e "CLOUDSMITH_STABLE_REPO=$CLOUDSMITH_STABLE_REPO" \
+        -e "CLOUDSMITH_BETA_REPO=$CLOUDSMITH_BETA_REPO" \
+        -e "CLOUDSMITH_UNSTABLE_REPO=$CLOUDSMITH_UNSTABLE_REPO" \
+        -e "CIRCLE_BUILD_NUM=$CIRCLE_BUILD_NUM" \
+        -e "TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER" \
+    $DOCKER_IMAGE /bin/bash -xe /ci-source/build.sh
+rm -f $TRAVIS_BUILD_DIR/build.sh
 
 
 # Install cloudsmith-cli (for upload) and cryptography (for git-push)
