b51f962e471f0d59f9d6edb74b9b54b04250f659
dsr <bdbcat@yahoo.com>
2021-02-23 23:45:43 -0500

    Update MacOS CircleCI build script

--- ci/circleci-build-macos.sh
+++ ci/circleci-build-macos.sh
@@ -1,41 +1,57 @@
 #!/usr/bin/env bash
 
 #
-# Build the Travis OSX artifacts 
+# Build the  MacOS artifacts
 #
 
-# bailout on errors and echo commands
+# Fix broken ruby on the CircleCI image:
+if [ -n "$CIRCLECI" ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
 set -xe
 
-# As of travis-ci-macos-10.13-xcode9.4.1-1529955246, the travis osx image
-# contains a broken homebrew. Walk-around by reinstalling:
-curl -fsSL \
-     https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh \
-    > uninstall.sh
-chmod 755 uninstall.sh
-./uninstall.sh -q -f
-
-inst="https://raw.githubusercontent.com/Homebrew/install/master/install"
-/usr/bin/ruby -e "$(curl -fsSL $inst)"
-
-
 set -o pipefail
-for pkg in cairo cmake libarchive libexif python3 wget; do
-    brew list $pkg 2>&1 >/dev/null || brew install $pkg
+# Check if the cache is with us. If not, re-install brew.
+brew list --versions libexif || brew update-reset
+
+for pkg in cairo cmake gettext libarchive libexif python wget; do
+    brew list --versions $pkg || brew install $pkg || brew install $pkg || :
+    brew link --overwrite $pkg || brew install $pkg
 done
 
-wget http://opencpn.navnux.org/build_deps/wx312_opencpn50_macos109.tar.xz
-tar xJf wx312_opencpn50_macos109.tar.xz -C /tmp
+#wget -q http://opencpn.navnux.org/build_deps/wx312_opencpn50_macos109.tar.xz - unsupported link, replaced with below
+curl -o wx312B_opencpn50_macos109.tar.xz https://download.opencpn.org/s/rwoCNGzx6G34tbC/download
+tar xJf wx312B_opencpn50_macos109.tar.xz -C /tmp
 export PATH="/usr/local/opt/gettext/bin:$PATH"
 echo 'export PATH="/usr/local/opt/gettext/bin:$PATH"' >> ~/.bash_profile
- 
+
+export MACOSX_DEPLOYMENT_TARGET=10.9
+
+#wget -q http://opencpn.navnux.org/build_deps/Packages.dmg - unsupported link, replaced with below
+#curl -o Packages.dmg https://download.opencpn.org/s/SneCR3z9XM3aRc6/download
+#hdiutil attach Packages.dmg
+#sudo installer -pkg "/Volumes/Packages 1.2.5/Install Packages.pkg" -target "/"
+# use brew to get Packages.pkg
+if brew list --cask --versions packages; then
+    version=$(pkg_version packages '--cask')
+    sudo installer \
+        -pkg /usr/local/Caskroom/packages/$version/packages/Packages.pkg \
+        -target /
+else
+    brew install --cask packages
+fi
+
 rm -rf build && mkdir build && cd build
-CI_BUILD=ON
-cmake -DOCPN_CI_BUILD=$CI_BUILD \
-  -DOCPN_USE_LIBCPP=ON \
-  -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wx312_opencpn50_macos109/bin/wx-config \
-  -DwxWidgets_CONFIG_OPTIONS="--prefix=/tmp/wx312_opencpn50_macos109" \
-  -DCMAKE_INSTALL_PREFIX= "/" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
+cmake \
+  -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wx312B_opencpn50_macos109/bin/wx-config \
+  -DwxWidgets_CONFIG_OPTIONS="--prefix=/tmp/wx312B_opencpn50_macos109" \
+  -DCMAKE_INSTALL_PREFIX= \
+  -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
+  "/" \
   ..
 make -sj2
 make package
+
+#make create-pkg
+
